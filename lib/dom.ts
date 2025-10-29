97b5ecf9203d74649398f5fdf900a1b87555e06d
Belati <belatijagadbintangsyuhada@gmail.com>
2025-10-29 21:39:29 +0700

    refactor(runtime):  implement vdom

--- lib/dom.ts
+++ lib/dom.ts
@@ -1,33 +1,52 @@
 type Props = { [key: string]: any };
-type Child = Node | string | number | null | undefined;
+type VChild = VNode | string | number | null | undefined;
+
+type VNode = {
+    tagName: string;
+    props: Props;
+    children: VChild[];
+}
+
+function isPropsObject(value: Props | VChild | undefined): value is Props {
+    if (typeof value !== 'object' || value == null || Array.isArray(value))
+        return false;
+
+    return !("children" in value);
+}
 
 export function f(
     tagName: string,
-    propsOrFirstChild?: Props | Child,
-    ...children: Child[]
+    propsOrFirstChild?: Props | VChild,
+    ...children: VChild[]
 ) {
-    const element = document.createElement(tagName);
-    let allChildren: Child[] = children ?? [];
+    let props: Props = {};
+    let allChildren: VChild[] = children ?? [];
 
-    if (
-        propsOrFirstChild
-        && typeof propsOrFirstChild == "object"
-        && !(propsOrFirstChild instanceof Node)
-    ) {
-        const props = propsOrFirstChild as Props;
-        for (const [key, value] of Object.entries(props))
-            element.setAttribute(key == "className" ? "class" : key, value);
+    if (isPropsObject(propsOrFirstChild)) {
+        props = propsOrFirstChild;
     } else if (propsOrFirstChild) {
         allChildren.unshift(propsOrFirstChild);
     }
 
-    for (const child of allChildren) {
-        if (typeof child == "string" || typeof child == "number") {
-            element.appendChild(document.createTextNode(child.toString()));
-        } else if (child instanceof Node) {
-            element.appendChild(child);
-        }
+    return { tagName, props, children: allChildren }
+}
+
+export function render(vnode: VChild, container: HTMLElement) {
+    if (vnode == null || vnode == undefined)
+        return;
+
+    if (typeof vnode === 'string' || typeof vnode === 'number') {
+        container.appendChild(document.createTextNode(vnode.toString()));
+        return;
     }
 
-    return element
+    const element = document.createElement(vnode.tagName);
+
+    for (const [key, value] of Object.entries(vnode.props))
+        element.setAttribute(key == 'className' ? 'class' : key, value)
+
+    for (const child of vnode.children)
+        render(child, element)
+
+    container.appendChild(element);
 }
